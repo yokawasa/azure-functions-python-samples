1f2a1e25ea332ab4d32a3e0c50bfa4d871649184
Yoichi Kawasaki <yoichika@microsoft.com>
2017-01-30 04:25:28 +0900

    initial entry: queue-trigger

---
+++ queue-trigger-rssfeed-crawler/README.md
@@ -0,0 +1,10 @@
+# queue-trigger-rssfeed-crawler
+Azure Functions Queue Trigger Python Sample that get RSS feed URL from Queue and dump all items that obtained from RSS feed
+
+
+## Add RSS feeds to Queue to trigger the function
+```
+https://azure.microsoft.com/en-us/blog/feed/
+https://azure.microsoft.com/en-us/updates/feed/
+...
+```
---
+++ queue-trigger-rssfeed-crawler/function/run.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.append("site-packages")
+import feedparser
+
+# Read the queue message
+rss_feed_url = open(os.environ['inputMessage']).read()
+print "Python script processes rss feed: '{0}'".format(rss_feed_url)
+
+# Get RSS feed by using feedparser module
+feed=feedparser.parse(rss_feed_url)
+
+# Dump RSS feed obtained
+print feed.feed.title.encode('utf-8')
+for entry in feed[ 'entries' ]:
+    print "Title: ", entry[ 'title' ].encode('utf-8')
+    print "URL: ", entry[ 'link' ]
+    print "Description: ", entry[ 'description' ].encode('utf-8')
+    print "Date: ", entry[ 'updated' ]
