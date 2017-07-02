78688764f129a2ec0addfc31638a0e601f21c01c
Yoichi Kawasaki <yoichika@microsoft.com>
2017-07-02 09:57:51 +0900

    Fixed api key header name -> x-functions-key

--- http-trigger-dump-request/send-https-requests.sh
+++ http-trigger-dump-request/send-https-requests.sh
@@ -6,13 +6,13 @@ api_key="AZURE_FUNCTION_KEY: ex. aRVQ7Lj0vzDhY0JBYF8gpxYyEBxLwhO51JSC7X5dZFbTvRO
 echo "Sending HTTP GET Request............."
 curl -s\
  -H "Content-Type: application/json; charset=UTF-8"\
- -H "api-key: ${api_key}"\
+ -H "x-functions-key: ${api_key}"\
  -XGET ${api_url} -d'foo=abc1111&bar=xyz2222'
 
 echo "Sending HTTP POST Request............."
 curl -s\
  -H "Content-Type: application/json; charset=UTF-8"\
- -H "api-key: ${api_key}"\
+ -H "x-functions-key: ${api_key}"\
  -XPOST ${api_url} -d'{
     "id: "00000121",
     "name": "Yoichi Kawasaki"
