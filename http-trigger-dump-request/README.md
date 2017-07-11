e3bacde9df49aa6d62cc52a5b8724e6c8eda4485
Yoichi Kawasaki <yoichika@microsoft.com>
2017-07-11 10:52:06 +0900

    add response: return all request info as json

--- http-trigger-dump-request/README.md
+++ http-trigger-dump-request/README.md
@@ -1,2 +1,7 @@
 # http-trigger-dump-request
 Azure Functions HTTP Trigger Python Sample that get and dump HTTPS request info that the trigger receives
+
+
+| Trigger | In/Out Bindings |
+------------ | ----------- |
+| HTTP Trigger | output:HTTP |
--- http-trigger-dump-request/function/run.py
+++ http-trigger-dump-request/function/run.py
@@ -12,13 +12,29 @@ https://github.com/anthonyeden/Azure-Functions-Python-HTTP-Example
 
 import sys
 import os
+import json
 
-DEFAULT_METHOD = "GET"
+_AZURE_FUNCTION_DEFAULT_METHOD = "GET"
+_AZURE_FUNCTION_HTTP_INPUT_ENV_NAME = "req"
+_AZURE_FUNCTION_HTTP_OUTPUT_ENV_NAME = "res"
+_REQ_PREFIX = "REQ_"
+
+def write_http_response(status, body_dict):
+    return_dict = {
+        "status": status,
+        "body": json.dumps(body_dict),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
+    output = open(os.environ[_AZURE_FUNCTION_HTTP_OUTPUT_ENV_NAME], 'w')
+    output.write(json.dumps(return_dict))
+
 
 env = os.environ
 
 # Get HTTP METHOD
-http_method = env['REQ_METHOD'] if env.has_key('REQ_METHOD') else DEFAULT_METHOD
+http_method = env['REQ_METHOD'] if env.has_key('REQ_METHOD') else _AZURE_FUNCTION_DEFAULT_METHOD
 print "HTTP METHOD => {}".format(http_method)
 
 # Get QUERY STRING
@@ -28,9 +44,14 @@ query_string = urlparts[1] if len(urlparts) == 2 else ''
 print "QUERY STRING => {}".format(query_string)
 
 if http_method.lower() == 'post':
-    request_body = open(env['req'], "r").read()
+    request_body = open(env[_AZURE_FUNCTION_HTTP_INPUT_ENV_NAME], "r").read()
     print "REQUEST BODY => {}".format(request_body)
 
+res_body = {}
 print "Dump ENVIRONMENT VARIABLES:"
 for k in env:
     print "ENV: {0} => {1}".format(k, env[k])
+    if (k.startswith(_REQ_PREFIX)):
+        res_body[k] = env[k]
+
+write_http_response(200, res_body)
