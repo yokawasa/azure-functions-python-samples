24038dc02b76c4a6be340a8f848734130040121e
Yoichi Kawasaki <yoichika@microsoft.com>
2017-11-07 21:13:33 +0900

    support both Python 2 and 3.X

--- http-trigger-dump-request/README.md
+++ http-trigger-dump-request/README.md
@@ -5,3 +5,7 @@ Azure Functions HTTP Trigger Python Sample that get and dump HTTPS request info 
 | Trigger | In/Out Bindings |
 ------------ | ----------- |
 | HTTP Trigger | output:HTTP |
+
+
+Support both Python 2 and 3.X
+
--- http-trigger-dump-request/function/run.py
+++ http-trigger-dump-request/function/run.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 """
-
 Azure Functions HTTP Trigger Python Sample
 - Get and dump HTTPS request info that the trigger receives
 
 Special Thanks to anthonyeden for great Python HTTP example:
 https://github.com/anthonyeden/Azure-Functions-Python-HTTP-Example
 
+Suppoert both Python 2 and 3.X
 """
 
 import os
@@ -33,23 +33,23 @@ def write_http_response(status, body_dict):
 env = os.environ
 
 # Get HTTP METHOD
-http_method = env['REQ_METHOD'] if env.has_key('REQ_METHOD') else _AZURE_FUNCTION_DEFAULT_METHOD
-print "HTTP METHOD => {}".format(http_method)
+http_method = env['REQ_METHOD'] if 'REQ_METHOD' in env else _AZURE_FUNCTION_DEFAULT_METHOD
+print("HTTP METHOD => {}".format(http_method))
 
 # Get QUERY STRING
-req_url = env['REQ_HEADERS_X-ORIGINAL-URL'] if env.has_key('REQ_HEADERS_X-ORIGINAL-URL') else ''
+req_url = env['REQ_HEADERS_X-ORIGINAL-URL'] if 'REQ_HEADERS_X-ORIGINAL-URL' in env else ''
 urlparts =req_url.split('?') 
 query_string = urlparts[1] if len(urlparts) == 2 else ''
-print "QUERY STRING => {}".format(query_string)
+print("QUERY STRING => {}".format(query_string))
 
 if http_method.lower() == 'post':
     request_body = open(env[_AZURE_FUNCTION_HTTP_INPUT_ENV_NAME], "r").read()
-    print "REQUEST BODY => {}".format(request_body)
+    print("REQUEST BODY => {}".format(request_body))
 
 res_body = {}
-print "Dump ENVIRONMENT VARIABLES:"
+print("Dump ENVIRONMENT VARIABLES:")
 for k in env:
-    print "ENV: {0} => {1}".format(k, env[k])
+    print("ENV: {0} => {1}".format(k, env[k]))
     if (k.startswith(_REQ_PREFIX)):
         res_body[k] = env[k]
 
