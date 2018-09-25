068a14a73a1fd3091fc5b824d7136c4e43f4683b
Yoichi Kawasaki <yoichika@microsoft.com>
2018-09-25 11:17:20 +0900

    Added a script to install the Azure CLI extension for the Azure Functions Linux Consumption preview

--- scripts/create-azfuncv2-linux.sh
+++ scripts/create-azfuncv2-linux.sh
@@ -8,9 +8,7 @@ set -e -x
 RESOURCE_GROUP="<RESOURCE GROUPP MAME>"
 REGION="<REGION NAME: eastus>"
 STORAGE_ACCOUNT="<STORAGE ACCOUNT NAME>"
-APP_SERVICE_PLAN="<APP SERVICE PLAN NAME>"
 APP_NAME="<FUNCTION APP NAME>" #  the name needs to be unique across all apps in Azure.
-DEPLOY_SOURCE_URL="https://github.com/Azure-Samples/functions-quickstart-linux"
 
 echo "Create Resource Group: $RESOURCE_GROUP"
 az group create --name $RESOURCE_GROUP --location $REGION
@@ -21,24 +19,13 @@ az storage account create --name $STORAGE_ACCOUNT \
     --resource-group $RESOURCE_GROUP \
     --sku Standard_LRS
 
-echo "Create a Linux App Service plan: $APP_SERVICE_PLAN"
-az appservice plan create --name $APP_SERVICE_PLAN \
+echo "Create a empty function app on Linux (Preview): $APP_NAME"
+az functionapp createpreviewapp --name $APP_NAME \
     --resource-group $RESOURCE_GROUP \
-    --sku B1 \
-    --is-linux
-
-echo "Create a empty function app on Linux: $APP_NAME"
-az functionapp create --name $APP_NAME \
     --storage-account $STORAGE_ACCOUNT \
-    --resource-group $RESOURCE_GROUP \
-    --plan $APP_SERVICE_PLAN 
-
-#echo "Create a function app on Linux based on deployment source: $APP_NAME"
-#az functionapp create --name $APP_NAME \
-#    --storage-account $STORAGE_ACCOUNT \
-#    --resource-group $RESOURCE_GROUP \
-#    --plan $APP_SERVICE_PLAN \
-#    --deployment-source-url https://github.com/Azure-Samples/functions-quickstart-linux
+    -l $REGION \
+    --runtime python \
+    --is-linux
 
 #echo "Clearning up all resources"
 # az group delete --name $RESOURCE_GROUP
