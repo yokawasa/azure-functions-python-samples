f8eb5325b03f09a0b207680c29ba4a8cff89bec8
Adrian Calinescu <foo@snobu.org>
2019-10-08 12:16:51 +0300

    Fix var name in service bus function
    ...i am a horrible programmer

--- v2functions/sbqueue-trigger-sbqueue-out-binding/__init__.py
+++ v2functions/sbqueue-trigger-sbqueue-out-binding/__init__.py
@@ -4,4 +4,4 @@ import azure.functions as func
 def main(msgIn: func.ServiceBusMessage, msgOut: func.Out[str]):
     body = msgIn.get_body().decode('utf-8')
     logging.info(f'Processed Service Bus Queue message: {body}')
-    msgOut.set(msgbody)
+    msgOut.set(body)
